c77558be76c764ccbf617d86847b0333d0a159fd
Tomas Caufriez <tomascaufriez@gmail.com>
2015-03-23 12:57:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
+app.get('/hello', function(req, res){
+	res.end("gg");
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function(){
+http.listen(80, function(){
   console.log('listening on *:3000');
 });
